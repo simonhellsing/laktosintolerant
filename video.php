b6346a87a903ce701c96ac59b357b14ed699e619
Simon Hellsing <simonhellsing@Simons-MacBook-Pro.local>
2014-02-03 16:19:10 +0100

    added all

---
+++ video.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Information om Laktosintolerans och hur man lever som laktosintolerant.">
+    <title>Laktosintolerans</title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <link rel="stylesheet" href="stylesheets/foundation-icons.css" />
+    <link href='http://fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet' type='text/css'>
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+
+    
+  
+
+
+  <div class="flex-video iv">
+
+    <iframe src="//player.vimeo.com/video/65475425" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="http://vimeo.com/65475425">OSCILLATE</a> from <a href="http://vimeo.com/user8071903">Daniel Sierra</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+
+  </div>
+  
+
+    <script src="bower_components/jquery/jquery.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/foundation/foundation.topbar.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
