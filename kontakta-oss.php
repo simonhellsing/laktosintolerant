ce9332a8fe76b4591e9e654fa70b957513cbd422
Simon Hellsing <simonhellsing@Simons-MacBook-Pro.local>
2014-02-03 16:08:15 +0100

    second commit

---
+++ kontakta-oss.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Information om Laktosintolerans och hur man lever som laktosintolerant.">
+    <title>Laktosintolerans</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <link rel="stylesheet" href="stylesheets/foundation-icons.css" />
+    <link href='http://fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200italic,300italic,200,300,400,600' rel='stylesheet' type='text/css'>
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+
+    
+  <header>
+    <div class="row">
+        <div class="small-12 medium-12 large-4 columns">
+          <h1>laktosintolerant</h1>
+          <div class="show-for-small icn right">
+            <a href="#" class="button"><i class="fi-list"></i></a>
+          </div>
+        </div>
+
+        <nav>
+          
+          <div class="hide-for-small medium-12 large-8 columns">
+            <ul class="button-group">
+              <li><a href="/laktos" class="button">Startsidan</a></li>
+              <li><a href="om-laktosintolerans" class="button">Om laktosintolerans</a></li>
+              <li><a href="symtom-for-laktosintolerans" class="button">Symtom för laktosintolerans</a></li>
+              <li><a href="laktosfri-mat" class="button">Laktosfri mat</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+  </header>
+<section class="content">
+  <div class="row">
+    <div class="small-12 columns">
+      <h1>Kontakta oss</h1>
+    </div>
+
+  </div>
+  <div class="row">
+    <div class="small-12 medium-6 columns">
+      <p>
+        Idén med Laktosintolerant.info är att leverera en välkonstruerad och praktisk informationskälla där du som användare kan läsa om laktosintolerans och laktos och allt vad det innebär.
+      </p>
+      <p>
+        Vi strävar alltid efter kvalitativt och uppdaterat innehåll, och välkommnar dig därför att höra av dig till oss om det finns något finns något du tycker behöver ändras eller läggas till på sidan.
+      </p>
+      
+    </div>
+    <div class="small-12 medium-6 columns">
+    <form>
+      <div class="row">
+
+        <div class="large-12 columns">
+
+
+          <label>Namn</label>
+          <input type="text" placeholder="Namn" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>E-mail</label>
+          <input type="text" placeholder="E-mail" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>Meddelande</label>
+          <textarea placeholder="Meddelande"></textarea>
+        </div>
+      </div>
+      <a action="submit" class="button small radius">Skicka</a>
+      </form>
+    </div>
+  </div>
+  </section>
+
+    
+
+  <footer>
+    <div class="row">
+    <div class="small-12 columns">
+      <div class="small-0 medium-4 columns">
+        <ul>
+          <li><a href="/laktos">Startsidan</a></li>
+          <li><a href="om-laktosintolerans">Om laktosintolerans</a></li>
+        </ul>
+      </div>
+      <div class="small-0 medium-4 columns">
+        <ul>
+          <li><a href="symtom-for-laktosintolerans">Symtom för laktosintolerans</a></li>
+          <li><a href="laktosfri-mat">Laktosfri mat</a></li>
+        </ul>
+      </div>
+      <div class="small-0 medium-4 special columns">
+        <ul>
+          <li class="dark"><a href="kontakta-oss">Kontakta Oss</a></li>
+          <li class="dark"><a href="sitemap">Sitemap</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </footer>
+  
+
+    <script src="bower_components/jquery/jquery.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/foundation/foundation.topbar.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
