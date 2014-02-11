6aab9ad2908f1c4f8136e08c59f3e1360d72a650
simonhellsing <simonhellsing@gmail.com>
2014-02-11 01:25:13 +0100

    added simonhellsing

--- config.rb
+++ config.rb
@@ -1,10 +1,9 @@
 # Require any additional compass plugins here.
-add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "scss"
+sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
